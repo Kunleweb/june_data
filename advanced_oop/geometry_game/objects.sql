4ced76e859a88b4c8e1356e77ca95f6b761b5cd1
unknown <aadeye302@caledonian.ac.uk>
2026-06-10 20:50:18 +0100

    june

--- advanced_oop/geometry_game/objects.sql
+++ advanced_oop/geometry_game/objects.sql
@@ -6,3 +6,35 @@ row_number() -- each row gets a uniques no
 -- use partition by to assign this bs within groups
 
 rank() over(partition by col_name) as ranked_col
+
+
+
+cte
+with order_counts as (
+
+select count(order_id) as total_orders from orders
+
+)
+
+
+
+-- select 
+--     order_id
+--     case 
+--     when order_id%2 !=0 and order_id != total_orders then order order_id+1
+--     when order_id%2 !=0 and order_id = total_orders then order order_id
+--     else order_id-1
+--     end as corrected_order_id, item
+--     from orders cross join order_counts 
+    
+
+SELECT
+  order_id,
+  CASE
+    WHEN order_id % 2 != 0 AND order_id != total_orders THEN order_id + 1 -- Line 1
+    WHEN order_id % 2 != 0 AND order_id = total_orders THEN order_id -- Line 2
+    ELSE order_id - 1 -- Line 3
+  END AS corrected_order_id,
+  item
+FROM orders
+CROSS JOIN order_counts
